ae8fbd9099d2627764831199390b5583561af36a
Milene Naiara Rebinski <108832930+MileneNaiara@users.noreply.github.com>
2024-06-05 15:12:45 -0300

    05-06

--- main.js
+++ main.js
@@ -2,21 +2,21 @@ const numeroSenha = document.querySelector('.parametro-senha-texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 const checkbox = document.querySelectorAll('.checkbox');
-const botoes = document.querySelector('.parametro-senha-botao');
+const botoes = document.querySelectorAll('.parametro-senha-botao');
 const forcaSenha = document.querySelector('.forca');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho(){
-    if(tamanhoSenha > 1){
-    // tamanhoSenha = tamanhoSenha - 1;
+    if (tamanhoSenha > 1){
+    // tamanhoSenha = tamanhoSenha-1;
     tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 function aumentaTamanho(){
-    if(tamanhoSenha < 20){
-    // tamanhoSenha = tamanhoSenha + 1;
+    if (tamanhoSenha < 20){
+    // tamanhoSenha = tamanhoSenha+1;
     tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
@@ -32,23 +32,23 @@ const simbolos = '!@%*?';
 const valorEntropia = document.querySelector('entropia');
 function geraSenha(){
     let alfabeto = '';
-    if (checkbox[0].checkbox){
+    if (checkbox[0].checked){
         alfabeto = alfabeto + letrasMaiusculas;
     }
-    if (checkbox[1].checkbox){
+    if (checkbox[1].checked){
         alfabeto = alfabeto + letrasMinusculas;
     }
-    if (checkbox[2].checkbox){
+    if (checkbox[2].checked){
         alfabeto = alfabeto + numeros;
     }
-    if (checkbox[3].checkbox){
+    if (checkbox[3].checked){
         alfabeto = alfabeto + simbolos;
     }
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++){
-    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    let numeroAleatorio = Math.random()*alfabeto.length;
     numeroAleatorio = Math.floor(numeroAleatorio);
-    senha = senha + letrasMaiusculas[numeroAleatorio];
+    senha = senha + alfabeto[numeroAleatorio];
     }
 campoSenha.value = senha;
 classificaSenha(alfabeto.length);
