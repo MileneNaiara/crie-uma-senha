c485a1d88c415676c643d71a58379305a66edabe
Milene Naiara Rebinski <108832930+MileneNaiara@users.noreply.github.com>
2024-06-11 11:26:35 -0300

    11-06

--- main.js
+++ main.js
@@ -54,7 +54,7 @@ campoSenha.value = senha;
 classificaSenha(alfabeto.length);
 }
 function classificaSenha(tamanhoAlfabeto){
-    let entropia = tamanhoSenha * Math.log2(alfabeto.length);
+    let entropia = tamanhoSenha * Math.log2(tamanhoAlfabeto);
     forcaSenha.classList.remove('fraca','media','forte');
     if (entropia > 57){
         forcaSenha.classList.add('forte');
